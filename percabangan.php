56af76ad792073aa92b4d9513d442bb24b641db4
Lasbi Noveni <63292554+lasbinoveni@users.noreply.github.com>
2023-09-22 21:08:48 +0700

    Belajar percabangan if else, ifelse ifelse.

---
+++ percabangan.php
@@ -0,0 +1,21 @@
+<?php
+
+// percabangan (if else, if else if else, ternary, switch)
+// if else
+$x = 10;
+if ($x < 20){
+	echo "benar <br>";
+} else {
+	echo "salah";
+}
+
+// ifelse ifelse
+$i = 20;
+if ($i < 10){
+	echo "benar";
+} else if($i == 15){
+	echo "bingo";
+} else {
+	echo "salah";
+}
+?>
