a34f112d54cf52f82d327dfc9334b8a97443e803
Lasbi Noveni <63292554+lasbinoveni@users.noreply.github.com>
2023-09-22 21:06:37 +0700

    Belajar pengulangan for, while.

---
+++ pengulangan.php
@@ -0,0 +1,16 @@
+<?php
+
+// pengulangan (for, while, do.. while, foreach..)
+
+// for
+for ($i=0; $i<5; $i++){
+	echo "hello Lasbi! <br> ";
+}
+
+// while
+$i = 0;
+while ($i < 5){
+	echo "hello php <br>";
+$i++;
+}
+
